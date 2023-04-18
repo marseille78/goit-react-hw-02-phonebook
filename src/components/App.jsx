abdde49198f721a829b65cdfcb5dd27c8aa9bbcf
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-04-19 02:12:48 +0300

    HW #2. Part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import ContactForm from './contact-form';
+import Filter from './filter';
+import ContactList from './contact-list';
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  getVisibleItems = () => {
+    const {contacts, filter} = this.state;
+    return contacts.filter(({ name }) => name.toLowerCase().indexOf(filter) >= 0)
+  }
+
+  handleFilter = (text) => {
+    this.setState({
+      filter: text
+    });
+  }
+
+  handleAddUser = (user) => {
+
+    const names = this.state.contacts.map(userItem => userItem.name);
+
+    if (names.includes(user.name)) {
+      alert(`${user.name} is already in contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [user, ...contacts]
+    }));
+  }
+
+  handleDeleteUser = (idUser) => {
+    this.setState(({contacts}) => ({
+      contacts: contacts.filter(({id}) => idUser !== id),
+    }));
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onAddUser={this.handleAddUser} />
+
+        <h2>Contacts</h2>
+        <Filter onFilter={this.handleFilter}/>
+        <ContactList visibleList={this.getVisibleItems()} onDeleteUser={this.handleDeleteUser}/>
+      </>
+    );
+  }
 };
